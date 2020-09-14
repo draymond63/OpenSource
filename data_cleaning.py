8cfe515ee1a189a4910b9a7cf13dfdbf8e126008
Daniel Raymond <dan@raymond.ch>
2020-09-13 21:23:02 -0400

    Initial Commit

---
+++ data_cleaning.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import ast
+
+def clean_data(df='commits.csv', repo_freq=3):
+    print('\nCLEANING THE DATA FROM GITHUB')
+    # Read file in if it is a file name
+    if isinstance(df, str):
+        df = pd.read_csv(df)
+    print(df.shape)
+
+    # * Break apart the author dict into its keys:
+    # name, email, time_sec, tz_ffset, date{seconds, nanos}
+    author_commits = {
+        'author': [],
+        'time_stamp': []
+    }
+    for entry in df['author']:
+        # Each entry has been pickled!
+        try:
+            entry = ast.literal_eval(entry)
+            # Copy over the name
+            author_commits['author'].append(entry['name'])
+            # Copy over the date
+            ts = entry['date']['seconds']
+            author_commits['time_stamp'].append(ts)                
+        except:
+            author_commits['author'].append(None)
+            author_commits['time_stamp'].append(None)
+            print("Bad author dict: ", entry)
+    # Convert dict to df
+    ac = pd.DataFrame(author_commits)
+    # Replace the author column in the og df
+    df.drop(['author', 'Unnamed: 0'], axis=1, inplace=True)
+    df = df.join(ac)
+
+    # * Convert the repo_name from a list to a single repo
+    def convert_list(x):
+        try: 
+            return ast.literal_eval(x)[0]
+        except: 
+            print("Bad repo_name: ", x)
+            return None
+    df['repo_name'] = df['repo_name'].apply(convert_list)
+
+    # * Last touches
+    df.dropna(inplace=True)
+    # Fix the date
+    # Convert the days-seconds to date-days
+    DATE_SHRINK_CONSTANT = 86400
+    df['time_stamp'] = df['time_stamp'] / DATE_SHRINK_CONSTANT
+    # Fix the date type and sort
+    df = df.astype({'time_stamp': int})
+    df.sort_values('time_stamp', ascending=False, inplace=True)
+    # Remove repos that don't appear the often
+    df = df[df.groupby('repo_name')['repo_name'].transform('count') >= repo_freq] 
+    # Save the data
+    df.to_csv('commits_cleaned.csv', index=False)
+    print(df.head())
+    print(df.shape)
+
+if __name__ == "__main__":
+    clean_data()
---
+++ repo_mapping.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def find_popular_repos(df='commits_cleaned.csv'):
+    print('\nMOST POPULAR REPOSITORIES BY THE NUMBER OF USERS')
+    # Read file in if it is a file name
+    if isinstance(df, str):
+        df = pd.read_csv(df)
+
+    print(df['repo_name'].nunique())
+    # Group the data by repo
+    repos = df.groupby('repo_name')
+    count = {
+        'repo_name': [],
+        'users': []
+    }
+    # Count the users involved in each repo
+    for name, repo in repos:
+        count['repo_name'].append(name)
+        count['users'].append(repo['author'].nunique())
+    
+    count = pd.DataFrame(count)
+    count.sort_values('users', ascending=False, inplace=True)
+
+    print(count.head())
+    print(count.shape)
+    return count
+
+
+if __name__ == "__main__":
+    find_popular_repos()
